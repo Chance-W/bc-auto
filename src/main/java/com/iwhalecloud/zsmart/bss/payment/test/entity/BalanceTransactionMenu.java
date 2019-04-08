03bb7c93361df525228db8e72e9c5cece5c16047
yuewenjuan <yue.wenjuan@ztesoft.com>
2019-04-08 14:07:47 +0800

    修改充值相关的功能，添加注释

--- src/main/java/com/iwhalecloud/zsmart/bss/payment/test/entity/BalanceTransactionMenu.java
+++ src/main/java/com/iwhalecloud/zsmart/bss/payment/test/entity/BalanceTransactionMenu.java
@@ -3,47 +3,63 @@ package com.iwhalecloud.zsmart.bss.payment.test.entity;
 import com.iwhalecloud.zsmart.bss.payment.test.intf.Menu;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import com.iwhalecloud.zsmart.bss.payment.test.entity.GetTime;
 
 import java.text.ParseException;
 
+
+
 public class BalanceTransactionMenu extends Menu {
 
-    //balance transaction界面进行余额查询
-    public void queryBal(String nbr){
+    /** balance transaction界面进行余额查询
+     * @author yue.wenjuan
+     * @version 1.0
+     * @CreateDate 2019-04-04
+     */
+        public void queryBal(String nbr){
         webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.name("custName")));
         driver.findElement(By.name("custName")).sendKeys(nbr);
         driver.findElement(By.className("js-search-customer")).click();
 
         webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("js-recharge")));
     }
-    //对查询出来的号码进行充值
-    public void recharge(String accNbr) {
 
-        driver.findElement(By.className("js-recharge")).click();
+    /**对查询出来的号码或者账户进行充值
+     * @author yue.wenjuan
+     * @version 1.0
+     * @CreateDate 2019-04-04
+     */
+    public void recharge(String accNbr,Boolean isAccNbrFlag) {
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("js-recharge"))).click();
 
         webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[3]/div[3]/div/button[1]")));
-        driver.executeScript("$('.ui-dialog').find('input[name=\"accNbr\"]').combobox('text', " + accNbr + ")");
+        if(isAccNbrFlag){
+            driver.executeScript("$('.ui-dialog').find('input[name=\"accNbr\"]').combobox('text', " + accNbr + ")");
+        }
 
         driver.executeScript("$('.js-charge-btn').find('button:first').click()");
         driver.executeScript("$('.modal-footer').find('button:contains(Recharge)').click()");
-
+        //选择Credit card
         webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("mt10")));
         driver.findElement(By.xpath("/html/body/div[4]/div/div[2]/div[2]/div/div[2]/button[2]")).click();
-
-        driver.findElement(By.className("js-payment")).click();
-        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("close")));
-
-        driver.executeScript("$('.ui-draggable-handle').find('button:first').click()");
+        //点击payment按钮充值
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("sub-title")));
+        driver.executeScript("$('.text-right').find('button:first').click()");
+        //等待充值结果
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("icon-success")));
     }
 
+    /**
+     * @author yue.wenjuan
+     * @version 1.0
+     * @CreateDate 2019-04-02
+     */
     public void qryHistroy (){
         driver.findElement(By.id("ui-id-4")).click();
         webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("js-form-query")));
         driver.findElement(By.xpath("//*[@id=\"balTransQuery\"]/form/div[3]/div/div/button[1]")).click();
 
     }
-    //增加账本
+
     public void addAccountBalance () throws ParseException {
 
         driver.findElement(By.xpath("//*[@id=\"tabs-acct-balance\"]/div/div/div[1]/div/div[4]/div/span[3]")).click();
@@ -67,8 +83,8 @@ public class BalanceTransactionMenu extends Menu {
 
 
 
-
     }
+
 
 
 }
--- src/test/java/PrepaidRecharge.java
+++ src/test/java/PrepaidRecharge.java
@@ -6,13 +6,20 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+
 public class PrepaidRecharge {
     @BeforeTest
     //程序初始化的一些操作
     public void setup() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "F:\\\\r90webtest\\drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver.exe");
     }
 
+
+    /**
+     * @author yue.wenjuan
+     * @version 1.0
+     * @CreateDate 2019 -04-04
+     */
     @Test
     public void testRecharge() throws Exception {
         ChromeDriver driver = new ChromeDriver();
@@ -20,14 +27,23 @@ public class PrepaidRecharge {
         WebDriverWait webDriverWait = new WebDriverWait(driver, 20);
         Portal portal = new Portal(driver, webDriverWait);
         portal.openUrl("http://172.16.24.140/portal-web/#");
-        portal.login("admin", "123");
+        portal.login("admin", "Uportal_123");
+        //通过号码充值
         BalanceTransactionMenu balanceTransactionMenu = portal.openMenu("balance transaction",BalanceTransactionMenu.class);
         balanceTransactionMenu.queryBal("201901260014");
-        balanceTransactionMenu.recharge("201901260014");
-        portal.closeUrl();
+        balanceTransactionMenu.recharge("201901260014",true);
+        portal.refreshPage();
+        //通过账户充值
+        portal.openMenu("balance transaction",BalanceTransactionMenu.class);
+        balanceTransactionMenu.queryBal("0000005697");
+        balanceTransactionMenu.recharge("0000005697",false);
+
+//        portal.closeUrl();
     }
 
+
     @AfterTest
+
     public void clear(){
 
     }
